a1862a7cdd3a1469931591e7956a78781e61844c
roger <rogerferraro256@gmail.com>
2021-05-01 18:22:14 -0300

    finished, wtf

--- binary.c
+++ binary.c
@@ -10,16 +10,46 @@
  *       Revision:  none
  *       Compiler:  gcc
  *
- *         Author:  Dr. Fritz Mehner (mn), mehner@fh-swf.de
- *        Company:  FH Südwestfalen, Iserlohn
+ *         Author:  Roger Salvadeo
  *
  * =====================================================================================
  */
 
 #include <stdio.h>
+#define MAX_SIZE 100
 
+// dear reader, while i wrote this, I was on drugs, so i don't remember anything, don't blame me
+// when it breaks
+int toBinary(int num)
+{
+int number = num;
+int a[1000];
+int i ;
 
-int main(){
-printf("teste");
+for (i= 0; number > 0; i++)
+{
+    a[i]=number%2;
+    number = number/2;
+}
+
+for (i=i-1; i >= 0; i--)
+{
+    printf("%d",a[i]);
+}
+
+printf("\n");
 return 0;
 }
+
+
+
+
+int main()
+{
+    int num;
+    scanf("%d",&num);
+
+    printf("\n %d",toBinary(num));
+    return 0;
+}
+
