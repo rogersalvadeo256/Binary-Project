9596396d031189f47b9f0efcf0c40b2b3ef62d47
roger <rogerferraro256@gmail.com>
2021-05-01 11:34:12 -0300

    shit

---
+++ binary.c
@@ -0,0 +1,25 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  binary.c
+ *
+ *    Description: teste 
+ *
+ *        Version:  1.0
+ *        Created:  01/05/2021 11:32:24
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Dr. Fritz Mehner (mn), mehner@fh-swf.de
+ *        Company:  FH Südwestfalen, Iserlohn
+ *
+ * =====================================================================================
+ */
+
+#include <stdio.h>
+
+
+int main(){
+printf("teste");
+return 0;
+}
